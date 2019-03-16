7293ae1c3eb2bbd014a4d77182616a12f691e548
yaxin1437 <897276786@qq.com>
2019-03-16 21:31:36 +0800

    临时数据增加文件夹存放

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,18 +1,41 @@
 // pages/history/history.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+   
+  },
+  longPress: function () {
+    wx.showModal({
+      title: '提醒',
+      content: '你将清空小程序本地所有缓存！',
+      success: function (res) {
+        if (res.confirm) { //判断用户是否点击了确定
+          wx.clearStorageSync();
+        }
+      }
+    })
+  },
 
+  execSetStorageSync: function () { //同步临时数据到缓存
+    var res = require("../temp_for_test/data.js").temp_data
+    wx.setStorageSync("temp_data", res)
+  },
+  getAllTempData: function () {//从缓存获取数据
+    var res = wx.getStorageSync("temp_data");
+    console.log(res);
+    return res;
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //this.execSetStorageSync()    //屏蔽临时数据，直接操作缓存数据
+    this.setData({
+      hisList: this.getAllTempData()
+    })
   },
 
   /**
