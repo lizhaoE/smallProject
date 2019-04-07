0c42f96bf289e44f8787777987aaad087054934e
yaxin1437 <897276786@qq.com>
2019-04-07 22:35:06 +0800

    拍照相册选取提取公共函数，避免代码重复

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,6 +18,48 @@ Page({
     
     })
   },
+  successSearch: function (res) {           //排查&相册上传查询公共函数
+    var _this = this;
+    var tempFilePaths = res.tempFilePaths; // 返回选定照片的本地文件路径列表
+    wx.getFileSystemManager().readFile({
+      filePath: tempFilePaths[0], //选择图片返回的相对路径
+      encoding: 'base64', //编码格式
+      success: res => { //成功的回调
+        var imageStr = res.data;
+        //  console.log('data:image/png;base64,' + res.data)
+        var url = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/plant';
+        wx.getStorage({
+          key: 'baidu_token',
+          success: function (res) {
+            var token = res.data;
+            // console.log('token,' + token);
+            _this.waitFuc();
+            wx.request({
+              url: url + '?access_token=' + token,
+              header: {
+                'content-type': 'application/x-www-form-urlencoded' // 默认值
+                //'content-type': 'application/json' //返回json数据
+              },
+              data: {
+                'image': imageStr,
+                'baike_num': 1
+              },
+              method: 'POST',
+              success(res) {
+                _this.execSetStorage(res)
+              },
+              fail: function () {
+                wx.navigateTo({
+                  url: '/pages/results/fail/fail',
+                })
+              }
+
+            })
+          },
+        })
+      }
+    })
+  },
 
   waitFuc: function () {  //等待函数
     wx.showLoading({
@@ -30,110 +72,30 @@ Page({
    
   //拍照图片事件处理函数
   camera_pic_serarch: function (callbackData){
-    var _this = this;
+    var that = this;
     wx.chooseImage({
       count: 1, // 限定只能选择一张照片
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图
       sourceType: ['camera'], // 指定选择相机
       success: function (res) {
-
-        // 返回选定照片的本地文件路径列表
-        var tempFilePaths = res.tempFilePaths;
-        wx.getFileSystemManager().readFile({
-          filePath: tempFilePaths[0], //选择图片返回的相对路径
-          encoding: 'base64', //编码格式
-          success: res => { //成功的回调
-            var imageStr = res.data;
-          //  console.log('data:image/png;base64,' + res.data)
-            var url = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/plant';
-            wx.getStorage({
-              key: 'baidu_token',
-              success: function(res) {
-                var token = res.data;
-               // console.log('token,' + token);
-                _this.waitFuc();
-                wx.request({
-                  url: url + '?access_token=' + token,
-                  header: {
-                    'content-type': 'application/x-www-form-urlencoded' // 默认值
-                    //'content-type': 'application/json' //返回json数据
-                  },
-                  data: {
-                    'image': imageStr,
-                    'baike_num': 1
-                  },
-                  method: 'POST',
-                  success(res) {
-                    _this.execSetStorage(res)
-                  },
-                  fail: function () {
-                    wx.navigateTo({
-                      url: '/pages/results/fail/fail',
-                    })
-                  }
-
-                }) 
-              },
-            })
-            
-
-          }
-        })
-        
+        that.successSearch(res)
       }
     })
   },
   
+
   //本地图片事件处理函数
   local_pic_search: function () {
-    var _this = this;
+    var that = this;
     wx.chooseImage({
       count: 1, // 限定只能选择一张照片
-      sizeType: ['compressed'], // 可以指定是原图还是压缩图
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图
       sourceType: ['album'], // 指定选择相册
       success: function (res) {
-
-        // 返回选定照片的本地文件路径列表
-        var tempFilePaths = res.tempFilePaths;
-        wx.getFileSystemManager().readFile({
-          filePath: tempFilePaths[0], //选择图片返回的相对路径
-          encoding: 'base64', //编码格式
-          success: res => { //成功的回调
-            var imageStr = res.data;
-            //  console.log('data:image/png;base64,' + res.data)
-            var url = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/plant';
-            wx.getStorage({
-              key: 'baidu_token',
-              success: function (res) {
-                var token = res.data;
-                // console.log('token,' + token);
-                wx.request({
-                  url: url + '?access_token=' + token,
-                  header: {
-                    'content-type': 'application/x-www-form-urlencoded' // 默认值
-                    //'content-type': 'application/json' //返回json数据
-                  },
-                  data: {
-                    'image': imageStr,
-                    'baike_num': 1
-                  },
-                  method: 'POST',
-                  success(res) {
-                    var data = JSON.stringify(res)
-                    console.log("图片百度返回内容" + JSON.parse(data).data);
-                  }
-                })
-              },
-            })
-
-
-          }
-        })
-
+        that.successSearch(res)
       }
     })
   },
-
 
   onLoad: function () {
   }
